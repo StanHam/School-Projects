7fa9bd882e6aafe967e0d5ff8e9054064b07c7d3
StanHam <43971608+StanHam@users.noreply.github.com>
2020-06-11 22:49:01 -0700

    Add files via upload

---
+++ COGS18FinalProject/test_functions.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import string
+
+#removes uncessary punctuation
+def remove_punctuation(input_string):
+    out_string = ''
+    
+    for item in input_string:
+        if item not in string.punctuation:
+            out_string += item
+        else:
+            continue
+            
+    return out_string
+
+def test_remove_punctuation():
+    assert remove_punctuation("I!!! want some??? sushi%^&") == "I want some sushi"
+
+
+# In[2]:
+
+
+result = test_remove_punctuation()
+print(result)
+
+
+# In[3]:
+
+
+#prepares text for interpreting by separating string into a list 
+#of words
+def prepare_text(input_string):
+    
+    temp_string = input_string.lower()
+    temp_string = remove_punctuation(temp_string)
+    out_list = temp_string.split()
+    
+    return out_list
+
+def test_prepare_text():
+    assert prepare_text("I want some sushi!!!") == ["i", "want","some","sushi"]
+    
+
+
+# In[ ]:
+
+
+result = test_prepare_text()
+print(result)
+
